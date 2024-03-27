995b14e0969c60aa8575c07a88ef74ecc9bf50a4
bmwrbsa <98557517+bmwrbsa@users.noreply.github.com>
2024-03-28 01:34:19 +0200

    added hugh lines to one

--- one.py
+++ one.py
@@ -58,3 +58,58 @@ ax[0].imshow(img_dog)
 ax[0].set_title('Original Image')
 ax[1].imshow(edge, cmap='gray')
 ax[1].set_title('Edges detected by Canny Filter')
+
+
+
+
+import sys
+import cv2 as cv
+import numpy as np
+import math
+
+
+#hough lines transfomation:
+default_file = 'dog.jpg'
+filename = sys.argv[1] if len(sys.argv) > 1 else default_file
+
+# Loads an image
+src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_GRAYSCALE)
+
+# Check if image is loaded fine
+if src is None:
+    print('Error opening image!')
+else:
+    print('Usage: hough_lines.py [image_name -- default ' + default_file + ']')
+
+    dst = cv.Canny(src, 50, 200, None, 3)
+
+    # Copy edges to the images that will display the results in BGR
+    cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
+    cdstP = np.copy(cdst)
+
+    lines = cv.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
+
+    if lines is not None:
+        for i in range(len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+            cv.line(cdst, pt1, pt2, (0, 0, 255), 3, cv.LINE_AA)
+
+    linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
+
+    if linesP is not None:
+        for i in range(len(linesP)):
+            l = linesP[i][0]
+            cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
+
+    cv.imshow("Source", src)
+    cv.imshow("Detected Lines (in red) - Standard Hough Line Transform", cdst)
+    cv.imshow("Detected Lines (in red) - Probabilistic Line Transform", cdstP)
+
+    cv.waitKey()
